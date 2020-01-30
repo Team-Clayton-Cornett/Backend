76d16f3e02e7ee34647b5d14f1806474a30d7603
Josh Westbrook <jwgy5@mail.missouri.edu>
2020-01-30 23:30:06 +0000

    added example REST Views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+
+# REST Framework: https://www.django-rest-framework.org/
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
 
 class HelloWorldView(APIView):
     permission_classes = (IsAuthenticated,)
@@ -13,3 +14,35 @@ class HelloWorldView(APIView):
         }
 
         return Response(content)
+
+"""
+Example Class-Based REST View:
+
+class ExampleView(APIView):
+    # if authentication is required
+    permission_classes = (IsAuthenticated,)
+
+    # for a get method
+    def get(self, request):
+        # initialize content dictionary
+        content = {}
+
+        # perform logic
+        # to obtain parameters from GET request
+        request.query_params['param_name']
+
+        # return response
+        return Response(content)
+
+    # for a POST method
+    def post(self, request):
+        # initialize content dictionary
+        content = {}
+
+        # perform logic
+        # to obtain parameters from POST request
+        request.data['param_name']
+
+        # return response
+        return Response(content)
+"""
