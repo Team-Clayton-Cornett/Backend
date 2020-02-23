ebe188ab35f25d6b816175ce1c38dbb5429f4d14
Shelby Heffron <smhgzd@mail.missouri.edu>
2020-02-22 21:30:45 -0600

    Removed unused imports

--- api/management/commands/xgboost_daily.py
+++ api/management/commands/xgboost_daily.py
@@ -1,13 +1,11 @@
 import csv
 import math
-import random
 
 from django.core.management.base import BaseCommand, CommandError
 from numpy import loadtxt
 from xgboost import XGBClassifier
 from datetime import date
 from enum import IntEnum 
-from numpy import loadtxt
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
